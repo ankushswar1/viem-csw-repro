402a1195dbbad863cb138e4797805a174442556e
Danny Bang <150051745+dabangarang2@users.noreply.github.com>
2024-06-27 14:35:00 -0400

    chainge to mainnet

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,58 @@
-'use client';
+"use client";
 import { createPublicClient, http } from "viem";
-import { zora } from "viem/chains";
-import {message, signature, address} from '@/lib/siwe';
+import { mainnet } from "viem/chains";
+import { message, signature, address } from "@/lib/siwe";
 import { useState } from "react";
 
-
 export default function Home() {
-  const [clientSideVerification, setClientSideVerification] = useState<boolean | undefined>();
-  const [apiSideVerification, setApiSideVerification] = useState<boolean | undefined>();
+  const [clientSideVerification, setClientSideVerification] = useState<
+    boolean | undefined
+  >();
+  const [apiSideVerification, setApiSideVerification] = useState<
+    boolean | undefined
+  >();
 
   // Verifying the signature from client-side code
   const onVerifyMessage = async () => {
-    const client = createPublicClient({chain: zora, transport: http()});
-    const isValid = await client.verifyMessage({message, signature, address});
+    const client = createPublicClient({ chain: mainnet, transport: http() });
+    const isValid = await client.verifyMessage({ message, signature, address });
     setClientSideVerification(isValid);
 
     setTimeout(() => {
-      setClientSideVerification(undefined)
+      setClientSideVerification(undefined);
     }, 2000);
-  }
+  };
 
   // Calling a NextJS API handler to verify the signature. See `@/app/api/route.ts`
   // for the verification logic there
   const onVerifyApi = async () => {
-    const res = await fetch('/api');
-    const {isValid} = await res.json();;
+    const res = await fetch("/api");
+    const { isValid } = await res.json();
     setApiSideVerification(isValid);
 
     setTimeout(() => {
-      setApiSideVerification(undefined)
+      setApiSideVerification(undefined);
     }, 2000);
-  }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <button onClick={() => onVerifyMessage()}>Verify signature client-side</button>
-        {clientSideVerification === true && 'Client-side signature verified correctly'}
-        {clientSideVerification === false && 'Client-side signature did not verify correctly'}
+        <button onClick={() => onVerifyMessage()}>
+          Verify signature client-side
+        </button>
+        {clientSideVerification === true &&
+          "Client-side signature verified correctly"}
+        {clientSideVerification === false &&
+          "Client-side signature did not verify correctly"}
         <br />
-        <button onClick={() => onVerifyApi()}>Verify signature in API handler</button>
-        {apiSideVerification === true && 'API-side signature verified correctly'}
-        {apiSideVerification === false && 'API-side signature did not verify correctly'}
+        <button onClick={() => onVerifyApi()}>
+          Verify signature in API handler
+        </button>
+        {apiSideVerification === true &&
+          "API-side signature verified correctly"}
+        {apiSideVerification === false &&
+          "API-side signature did not verify correctly"}
       </div>
     </main>
   );
